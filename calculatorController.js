ae8db92cc7dc04dd25eb5bf2c4ae9b066785b600
Hayden-Fade61 <63241537+Hayden-Fade61@users.noreply.github.com>
2024-08-27 10:51:56 +0200

    Added an input checker funtion

--- calculatorController.js
+++ calculatorController.js
@@ -4,36 +4,38 @@ function parseInput(expression){
   // Clean off whitespace
   // Throw error 2 if any illegal characters
   // Reverse final string for left to right rule
-  const ILLEGAL_CHARACTERS = /[^\d\(\)+\-*/.%]/
-  const IMPROPER_OPERATOR_REGEX = /[/*]{2,}/
-  try {
-    if (expression == "") throw "ERROR: No input";
+    if (expression == "") return "";
     expression = expression.replaceAll(/\s/g, "");
     expression = expression.replaceAll('\u00f7', '/');
     expression = expression.replaceAll('\u00d7', '*');
-    if (!validParentheses(expression)) throw "Syntax Error";
-    if (ILLEGAL_CHARACTERS.test(expression)) throw "Syntax Error";
-    if (IMPROPER_OPERATOR_REGEX.test(expression)) throw "Syntax Error";
-    return evaluate(expression.split(/(?=[+-/*]) | (?<=[+-/*])/g).reverse().join('')); // Lookarounds are goated
-  } 
+    let inputValid = checkInput(expression);
+    if (inputValid != "Valid") return inputValid;
+    return evaluate(expression.split(/(?=[+-/*%]) | (?<=[+-/*%])/g).reverse().join('')); // Lookarounds are goated
+}
+
+function checkInput(expression) {
+ try {
+  const ILLEGAL_CHARACTERS = /[^\d\(\)+\-*/.%]/
+  const IMPROPER_OPERATOR_USE = /([/*]{2,})|(^[/*%])|([+\-*/.%]$)|([+-][*/])/ 
+  if (ILLEGAL_CHARACTERS.test(expression)) throw "Syntax Error";
+  if (IMPROPER_OPERATOR_USE.test(expression)) throw "Syntax Error";
+  if (!validParentheses(expression)) throw "Syntax Error";
+  return "Valid";
+  }
   catch (error) {
     return error;
   }
 }
 
-function validParentheses(expression) {
+function validParentheses(expression){
   let stack = [];
   for(let i = 0; i < expression.length; i++){
-    console.log(stack);
-    console.log(expression[i]);
     if (expression[i] == '(') {
       stack.unshift('(');
     }
-    else if (expression[i] == ')' && stack.shift() == undefined) {
-      console.log("False");
+    else if (expression[i] == ')' && stack.shift() == undefined){
       return false
     }
   }
-  console.log(stack);
   return stack.length == 0;
 }
