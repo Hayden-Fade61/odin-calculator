627251c9705aef48a06e32a7e709e4a7c7d203de
Hayden-Fade61 <63241537+Hayden-Fade61@users.noreply.github.com>
2024-08-27 08:37:41 +0200

    I need a unit testing system that works with my browser

--- tests/parentheses.spec.js
+++
@@ -1,8 +0,0 @@
-// const calculator = require("../calculator");
-
-// describe("Handling Parentheses within expressions", function () {
-//   test("Parentheses 1", function () {
-//       expect(calculator.calculate("7/(5*2-(6/3))")).toEqual(0.875);
-//     }
-//   );
-// });
